de65c0441f0a12ad48699b0d9578014812ae89a4
Pritam Saha <pritamsahapri@gmail.com>
2025-07-26 19:32:50 +0530

    cors added

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ import './Connection/conn.js'
 import AuthRoutes from './Routes/user.route.js';
 import VideoRoutes from './Routes/video.route.js'
 import CommentRoutes from './Routes/comment.route.js'
+import cors from 'cors'
 
 const app = express()
 
 const PORT = 8000
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.use(express.json())
 app.use(cookieParser())
